34332687c131dca85ec73b5a8d30d1a7e629f5d3
Gaurav <gauravbhagat326@gmail.com>
2025-01-04 18:08:07 +0530

    whole revew setrtings and bootstrap website

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedCampgrounds = async () => {
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location: `${cities[random1000].city},${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://picsum.photos/400?random=${Math.random()}',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quae, repellendus. Quisquam, quidem. Consequatur, quos. Quisquam, quidem. Consequatur, quos.',
+            price
         })
         await camp.save();
     }
